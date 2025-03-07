10c487bf29be086b924142d87d08bcade413bee0
ultratrikx <rohanthm@outlook.com>
2025-03-07 16:24:32 -0500

    refactor: remove unused Footer import and clean up footer component

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 
 export function Footer() {
@@ -102,5 +101,3 @@ export function Footer() {
         </footer>
     );
 }
-
-export default Footer;
